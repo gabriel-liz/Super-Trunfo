95091d3baffdd30485b2e35abdcba65ea2b611de
gabriel-liz <gabriel.liz@outlook.com.br>
2021-04-13 18:03:00 -0300

    Jogo de super-trunfo

---
+++ assets/js.js
@@ -0,0 +1,425 @@
+var cartaKombi = {
+    nome: "Kombi",
+    imagem: "https://i.pinimg.com/564x/3b/24/50/3b2450021ea1a3dce2f0a82184b2df17.jpg",
+    atributos: {
+        "Velocidade Max": 50,
+        Cilindradas: 1390,
+        "0 à 100": 16.1,
+        Potência: 57,
+        Comprimento: 4.505
+    }  
+}
+var cartaGolf = {
+    nome: "Golf",
+    imagem: "https://i.pinimg.com/564x/85/52/60/855260d092d1f4db1a0eae222756802c.jpg",
+    atributos: {
+        "Velocidade Max": 227,
+        Cilindradas: 1781,
+        "0 à 100": 7.8,
+        Potência: 180,
+        Comprimento: 4.147
+    }
+}
+var cartaGol = {
+    nome: "Gol Power",
+    imagem: "https://i.pinimg.com/564x/f5/9f/22/f59f228f58058ae51a95c0ee26fa9b26.jpg",
+    atributos: {
+        "Velocidade Max": 189,
+        Cilindradas: 1781,
+        "0 à 100": 10.1,
+        Potência: 103,
+        Comprimento: 3.931
+    } 
+}
+var cartaFox = {
+    nome: "Fox Sportline",
+    imagem: "https://i.pinimg.com/564x/ad/01/8e/ad018e4d38b986e86a1a7734852ac8a1.jpg",
+    atributos: {
+        "Velocidade Max": 186,
+        Cilindradas: 1781,
+        "0 à 100": 7.8,
+        Potência: 180,
+        Comprimento: 4.147
+    }  
+}
+var cartaPolo = {
+    nome: "Polo",
+    imagem: "https://i.pinimg.com/564x/59/78/87/597887221e9273e0216730f2cf1be20f.jpg",
+    atributos: {
+        "Velocidade Max": 187,
+        Cilindradas: 1599,
+        "0 à 100": 12,
+        Potência: 107,
+        Comprimento: 4.129
+    }  
+}
+var cartaSaveiro = {
+    nome: "Saveiro Cross",
+    imagem: "https://i.pinimg.com/564x/03/2d/71/032d7142b20fce4cf3a3edc7972d7877.jpg",
+    atributos: {
+        "Velocidade Max": 175,
+        Cilindradas: 1781,
+        "0 à 100": 10.02,
+        Potência: 103,
+        Comprimento: 4.529
+    } 
+}
+var cartaSpace = {
+    nome: "Space Fox",
+    imagem: "https://i.pinimg.com/564x/cf/47/b8/cf47b882c4398468cec4b2c42ae9ee3b.jpg",
+    atributos: {
+        "Velocidade Max": 173,
+        Cilindradas: 1599,
+        "0 à 100": 11.1,
+        Potência: 101,
+        Comprimento: 4.180
+    }  
+}
+var cartaBora = {
+    nome: "Bora",
+    imagem: "https://combustivel.app/imgs/t650/consumo-bora-2-0.jpg",
+    atributos: {
+        "Velocidade Max": 195,
+        Cilindradas: 1984,
+        "0 à 100": 11.0,
+        Potência: 116,
+        Comprimento: 4.376
+    }  
+}
+var cartaCross = {
+    nome: "CrossFox",
+    imagem: "https://i.pinimg.com/564x/60/10/15/601015882b069a0272595dc1d7783be7.jpg",
+    atributos: {
+        "Velocidade Max": 175,
+        Cilindradas: 1599,
+        "0 à 100": 11,
+        Potência: 101,
+        Comprimento: 4.08
+    }  
+}
+var cartaJetta = {
+    nome: "Jetta",
+    imagem: "https://i.pinimg.com/564x/dd/af/13/ddaf138544054209a53d16f1b98622aa.jpg",
+    atributos: {
+        "Velocidade Max": 208,
+        Cilindradas: 2480,
+        "0 à 100": 9.6,
+        Potência: 150,
+        Comprimento: 4.554
+    }
+  
+}
+var cartaParati = {
+    nome: "Parati",
+    imagem: "https://i.pinimg.com/564x/e1/66/90/e16690772f1dbbc2ff19cb089e964034.jpg",
+    atributos: {
+        "Velocidade Max": 190,
+        Cilindradas: 1781,
+        "0 à 100": 10.1,
+        Potência: 103,
+        Comprimento: 4.189
+    }  
+}
+var cartaVariant = {
+    nome: "Variant",
+    imagem: "https://i.pinimg.com/564x/92/bd/1d/92bd1d0cfd757e3fac3cd18ec29cf3fc.jpg",
+    atributos: {
+        "Velocidade Max": 227,
+        Cilindradas: 1984,
+        "0 à 100": 7.9,
+        Potência: 200,
+        Comprimento: 4.774
+    }
+  
+}
+var cartaFusca = {
+    nome: "Fusca",
+    imagem: "https://i.pinimg.com/564x/fc/0c/98/fc0c980af0c19d33e28aeae47c31487b.jpg",
+    atributos: {
+        "Velocidade Max": 130,
+        Cilindradas: 1600,
+        "0 à 100": 20.5,
+        Potência: 50,
+        Comprimento: 3.500
+    }
+  
+}
+var cartaBeetle = {
+    nome: "New Beetle",
+    imagem: "https://i.pinimg.com/564x/85/f3/b2/85f3b2ba16c64505fa2e1bbd692cb8fb.jpg",
+    atributos: {
+        "Velocidade Max": 185,
+        Cilindradas: 1984,
+        "0 à 100": 10.9,
+        Potência: 116,
+        Comprimento: 4.129
+    }  
+}
+var cartaPhaeton = {
+    nome: "Phaeton",
+    imagem: "https://i.pinimg.com/564x/9f/18/15/9f18155d7bb9d1612a43c2a6887d2207.jpg",
+    atributos: {
+        "Velocidade Max": 250,
+        Cilindradas: 4921,
+        "0 à 100": 6.9,
+        Potência: 313,
+        Comprimento: 5.055
+    }  
+}
+var cartaTouran = {
+    nome: "Touran",
+    imagem: "https://i.pinimg.com/564x/b0/b1/8d/b0b18d978260dc642f8b8dbe6f7a65ed.jpg",
+    atributos: {
+        "Velocidade Max": 212,
+        Cilindradas: 1968,
+        "0 à 100": 9.1,
+        Potência: 172,
+        Comprimento: 4.391
+    }  
+}
+var cartaPassat  = {
+    nome: "Passat",
+    imagem: "https://i.pinimg.com/564x/22/42/a9/2242a90079a2c4f778e3563063c284b9.jpg",
+    atributos: {
+        "Velocidade Max": 230,
+        Cilindradas: 1984,
+        "0 à 100": 7.8,
+        Potência: 200,
+        Comprimento: 4.765
+    }
+}
+
+var baralhoMaquina = []
+var baralhoJogador = []
+var cartaMaquina
+var cartaJogador
+var rodadas = 1
+
+var cartas = [cartaKombi, cartaGolf, cartaGol, cartaFox, cartaPolo, cartaSaveiro, cartaSpace, cartaBora, cartaCross, cartaJetta, cartaParati, cartaVariant, cartaFusca, cartaBeetle, cartaPhaeton, cartaTouran, cartaPassat ]
+//            0           1           2          3         4            5            6           7     
+
+var pontosJogador = 0
+var pontosMaquina = 0
+
+atualizaPlacar()
+atualizaQuantidadeDeCartas()
+
+function atualizaQuantidadeDeCartas(){
+  var divQuantidadeCartasJogador = document.getElementById('quantidade-cartas-jogador')
+  var html = "Quantidade de cartas na mão do Jogador: " + baralhoJogador.length
+  divQuantidadeCartasJogador.innerHTML = html
+  
+  var divQuantidadeCartasMaquina = document.getElementById('quantidade-cartas-maquina')
+  var html = "Quantidade de cartas na mão da Maquina: " + baralhoMaquina.length
+  divQuantidadeCartasMaquina.innerHTML = html
+  
+}
+function atualizaPlacar(){
+  var divPlacar = document.getElementById('placar')
+  var html = "Jogador " + pontosJogador + "/" + pontosMaquina + " Maquina"
+  
+  divPlacar.innerHTML = html
+}
+
+function sortearCarta() {   
+    
+    auxCartas = cartas.slice()
+    var distribuirCartas = (auxCartas.length/2)
+    
+    for (var i = 0; i < distribuirCartas;i++){
+      var numeroCartaMaquina = parseInt(Math.random() * auxCartas.length)
+      var adicionar = baralhoMaquina.push(auxCartas[numeroCartaMaquina])
+      auxCartas.splice(numeroCartaMaquina,1)
+  
+      var numeroCartaJogador = parseInt(Math.random() * auxCartas.length)
+      adicionar =  baralhoJogador.push(auxCartas[numeroCartaJogador])
+      auxCartas.splice(numeroCartaJogador,1)
+      
+    }
+    atualizaPlacar()
+    atualizaQuantidadeDeCartas()
+    cartaJogador = baralhoJogador[0]
+    cartaMaquina = baralhoMaquina[0]
+  
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+  
+    atualizaQuantidadeDeCartas()
+    exibeCartaJogador()
+}
+
+function exibeCartaJogador() {
+    
+    var divCartaJogador = document.getElementById("carta-jogador")
+
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status'>"
+
+    divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName('atributo')
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {         
+            
+            return radioAtributo[i].value
+        }
+    }
+}
+
+function obtemAtributoSelecionadoMaquina() {
+      var radioAtributo = document.getElementsByName('atributo')
+     
+      var maiorValor = 1
+      var i = 0;
+    
+    for (var atributo in cartaMaquina.atributos){
+      
+      var valorTestado = cartaMaquina.atributos[atributo]
+      var teste = cartaMaquina.atributos
+    
+      if (valorTestado > maiorValor){
+        maiorValor = valorTestado
+        var pos = i 
+
+      }
+    i++
+    }
+    
+    return radioAtributo[pos].value
+}
+
+function jogar() {   
+    
+    var divResultado = document.getElementById("resultado")   
+    
+    if (rodadas%2 != 0){
+     
+    var atributoSelecionado = obtemAtributoSelecionado()
+     
+    
+    } else{
+      var atributoSelecionado = obtemAtributoSelecionadoMaquina() 
+       
+    }
+    
+    document.getElementById('btnJogar').disabled = true   
+  
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Venceu</p>'       
+        var adicionarFundoBaralho = baralhoJogador.push(cartaJogador)
+        var removerDoInicio = baralhoJogador.shift() 
+        var adicionarCartaAdv = baralhoJogador.push (cartaMaquina)
+        var removerCartaAdv = baralhoMaquina.shift()
+        pontosJogador++
+    } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Perdeu</p>'       
+        var adicionarFundoBaralho = baralhoMaquina.push(cartaMaquina)
+        var removerDoInicio = baralhoMaquina.shift() 
+        var adicionarCartaAdv = baralhoMaquina.push (cartaJogador)
+        var removerCartaAdv = baralhoJogador.shift()
+        pontosMaquina++
+    } else {
+        adicionarFundoBaralho = baralhoJogador.push(cartaJogador)
+        removerDoInicio = baralhoJogador.shift() 
+        adicionarFundoBaralho = baralhoMaquina.push(cartaMaquina)
+        removerDoInicio = baralhoMaquina.shift()       
+        htmlResultado = '<p class="resultado-final">Empatou</p>'
+    }    
+  
+  if (baralhoMaquina.length == 0){
+    atualizaQuantidadeDeCartas()
+    alert("Fim de jogo")
+    
+    htmlResultado = '<p class="resultado-final">VOCÊ EVITOU A INVOCAÇÃO DO MAL</p>'
+    document.getElementById('btnNovoJogo').disabled = false
+    
+  }else if (baralhoJogador.length ==0){
+    atualizaQuantidadeDeCartas()
+    alert("Fim de jogo")
+
+    htmlResultado = '<p class="resultado-final">VOCÊ NÃO EVITOU A INVOCAÇÃO DO CHTULHU</p>'
+    document.getElementById('btnNovoJogo').disabled = false
+    }
+  else {
+    document.getElementById('btnProximaRodada').disabled = false
+  }
+
+    divResultado.innerHTML = htmlResultado    
+  
+    atualizaPlacar()
+    exibeCartaMaquina()
+    document.getElementById(atributoSelecionado).style.backgroundColor = "red"
+   
+    cartaJogador = baralhoJogador[0]
+    cartaMaquina = baralhoMaquina[0]
+}
+
+function exibeCartaMaquina() {
+
+    var divCartaMaquina = document.getElementById("carta-maquina")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+    var opcoesTexto = ""
+    var i = 0
+    for (var atributo in cartaMaquina.atributos) {
+        
+        opcoesTexto += "<p type='text' id ='"+atributo+"'  name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status --spacing'>"
+
+    divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function proximaRodada(){    
+    cartaJogador = baralhoJogador[0]
+    cartaMaquina = baralhoMaquina[0]
+  var divCartas = document.getElementById('cartas')
+  divCartas.innerHTML = `<div id="carta-jogador" class = "carta"></div> <div id = "carta-maquina" class = "carta"></div>`
+  
+  if (rodadas%2 != 0){
+  document.getElementById('btnJogar').disabled = false
+  var divButton = document.getElementById('btnJogar')
+  document.getElementById('btnProximaRodada').disabled = true
+  divButton.value = "TURNO PC"
+   rodadas++
+   }
+  else{
+  var divButton = document.getElementById('btnJogar')
+  divButton.value = "JOGAR"
+  document.getElementById('btnJogar').disabled = false
+  document.getElementById('btnProximaRodada').disabled = true
+  rodadas++
+  }
+  
+  atualizaQuantidadeDeCartas()
+  exibeCartaJogador()
+
+  var divResultado = document.getElementById('resultado')
+  divResultado.innerHTML = ""
+}
+
+function novoJogo(){
+   pontosJogador = 0
+   pontosMaquina = 0
+   baralhoMaquina = []
+   baralhoJogador = []
+   cartaJogador = baralhoJogador[0]
+   cartaMaquina = baralhoMaquina[0]
+    atualizaPlacar()
+  atualizaQuantidadeDeCartas()
+  var divCartas = document.getElementById('cartas')
+    divCartas.innerHTML = `<div id="carta-jogador" class = "carta"></div> <div id = "carta-maquina" class = "carta"></div>`
+    document.getElementById('btnSortear').disabled = false
+}
